4d84e76aaae6aadd963cb474c7de7c669df47af5
Unknown <matheus29672967@hotmail.com>
2019-08-03 11:12:33 -0300

    more prints, let's see why ain't it getting the channel's ID

--- old_new_nexus.py
+++ old_new_nexus.py
@@ -35,7 +35,8 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    print("Received message!")
+    print("Received message from channel " + message.channel)
+    print("Channel id: " + message.channel.id)
     if message.channel.id == "599640898233565198":
         print("Message is in the right channel!")
         print("Trying to matching message to a role... Message:" + message.content)
